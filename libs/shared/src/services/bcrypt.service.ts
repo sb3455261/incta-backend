6231b0f57354b5347627228500d9fdb5eb259ff6
sb3455261 <sb3455261@gmail.com>
2024-08-05 02:01:23 +0000

    Test

--- libs/shared/src/services/bcrypt.service.ts
+++ libs/shared/src/services/bcrypt.service.ts
@@ -1,5 +1,5 @@
 import { Injectable } from '@nestjs/common';
-import * as bcrypt from 'bcrypt';
+import * as crypto from 'crypto';
 
 @Injectable()
 export abstract class HashPasswordService {
@@ -11,10 +11,11 @@ export abstract class HashPasswordService {
 export class BcryptService implements HashPasswordService {
   async hash(rawPassword: string | Buffer): Promise<string> {
     try {
-        const salt = bcrypt.genSaltSync()
-        const hash = bcrypt.hashSync(rawPassword, salt);
-        console.debug(hash, 'hash')
-        return hash
+        console.debug('hash 1')
+        const salt = crypto.randomBytes(16).toString('hex');
+        const hash = crypto.pbkdf2Sync(rawPassword, salt, 1000, 64, 'sha512').toString('hex');
+        console.debug('hash 2', `${salt}:${hash}`)
+        return `${salt}:${hash}`;
     } catch(error) {
         console.debug(error, 'error')
         return 'HASH'
@@ -25,6 +26,8 @@ export class BcryptService implements HashPasswordService {
     rawPassword: string | Buffer,
     encrypted: string,
   ): Promise<boolean> {
-    return bcrypt.compareSync(rawPassword, encrypted);
+    const [salt, storedHash] = encrypted.split(':');
+    const suppliedHash = crypto.pbkdf2Sync(rawPassword, salt, 1000, 64, 'sha512').toString('hex');
+    return storedHash === suppliedHash;
   }
 }
